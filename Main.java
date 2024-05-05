874cf85b4ca12095312c6d2987920b2a89b83dba
is-haka <isihakambo5@gmail.com>
2024-05-05 21:45:27 +0300

    JOptionInput dialog added

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello world");
+        //JOption Input dialog added
+        String input = JOptionPane.showInputDialog("Input your message");
+        //JOption message dialog added
+        JOptionPane.showMessageDialog(null, input);
     }
 }
